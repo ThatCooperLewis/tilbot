864943d63a3dc9ce8943fe3dc1acf31b1ed7ff25
ThatCooperLewis <cooper.lewis@gatherhere.com>
2020-01-14 02:48:17 -0500

    even better error parsing

--- tilbot.py
+++ tilbot.py
@@ -19,11 +19,9 @@ class GoogleSheets():
 
     def connect(self):
         self.cred = None
-        # Use existing token if available
         if os.path.exists(self.token_path):
             with open(self.token_path, 'rb') as token:
                 self.cred = pickle.load(token)
-        # Re-auth if no token
         if not self.cred or not self.cred.valid:
             if self.cred and self.cred.expired and self.cred.refresh_token:
                 self.cred.refresh(Request())
@@ -32,35 +30,41 @@ class GoogleSheets():
                     self.config['credentials_path'],
                     self.config['scope'])
                 self.cred = flow.run_local_server(port=0)
-            # Save the credentials for the next run
             with open(self.token_path, 'wb') as token:
                 pickle.dump(self.cred, token)
 
-    def get_dataframe(self, sheet_id):
+    def log_http_error(self, sheet_id):
+        print('Something went wrong during connection to GDrive')
+        print('Ensure the following URL is valid:')
+        print('https://docs.google.com/spreadsheets/d/' + sheet_id)
+        exit()
+
+    def column_from_table(self, table, column_name):
+        column_list = []
+        index = table[0].index(column_name)
+        for row in table[1:]:
+            column_list.append(row[index])
+        return column_list
+
+    def get_dataframe_from_table(self, sheet_id):
         sheet = build('sheets', 'v4', credentials=self.cred).spreadsheets()
         try:
-            result = sheet.values().get(spreadsheetId=sheet_id,
-                                        range=self.config['range']).execute()
+            result = sheet.values().get(
+                spreadsheetId=sheet_id,
+                range=self.config['range']).execute()
             table = result.get('values', [])
             if not table:
-                raise
+                raise Exception
         except:
-            print('Provided spreadsheet not accessible.')
-            print('Ensure the following URL is valid:')
-            print('https://docs.google.com/spreadsheets/d/' + sheet_id)
-            exit()
-        else:
-            id_list = []
-            id_index = table[0].index('id')
-            for row in table[1:]:
-                id_list.append(row[id_index])
-            return pd.DataFrame(table[1:], columns=table[0], index=id_list)
+            self.log_http_error(sheet_id)
+        id_list = self.column_from_table(table, 'id')
+        return pd.DataFrame(table[1:], columns=table[0], index=id_list)
 
 
 def main(sheet_config):
     sheets = GoogleSheets(sheet_config)
-    # Create pandas dataframe from GDrive sheet
-    tiller = sheets.get_dataframe(sheet_config['id'])
+    table_id = sheet_config['id']
+    tiller = sheets.get_dataframe_from_table(table_id)
     for id, transaction in tiller.iterrows():
         print(id, transaction.amount)
 
